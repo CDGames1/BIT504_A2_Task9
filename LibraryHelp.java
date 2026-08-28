20bc29bd881ea2e75c7547f65f9db0ab74d78b4c
Chiricahua Dilloway <63894306+CDGames1@users.noreply.github.com>
2026-08-28 22:12:28 +1200

    28 08 2026 added a function to display the search help

---
+++ LibraryHelp.java
@@ -0,0 +1,13 @@
+// name - Chiricahua Dilloway
+// Student ID -
+// Assessment 2 BIT504
+
+public class LibraryHelp {
+    public static void showAll() {
+        showSearchHelp();
+    }
+
+    public static void showSearchHelp() {
+        System.out.println("search by entering part of a book title");
+    }
+}
