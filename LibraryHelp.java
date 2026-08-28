390f86c08002aa4d4c5b6c3ac41e806de77f27a1
Chiricahua Dilloway <63894306+CDGames1@users.noreply.github.com>
2026-08-28 22:12:37 +1200

    28 08 2026 added a function to display the loan help

--- LibraryHelp.java
+++ LibraryHelp.java
@@ -5,9 +5,14 @@
 public class LibraryHelp {
     public static void showAll() {
         showSearchHelp();
+        showLoanHelp();
     }
 
     public static void showSearchHelp() {
         System.out.println("search by entering part of a book title");
+    }
+
+    public static void showLoanHelp() {
+        System.out.println("use option 3 to manage book loans");
     }
 }
