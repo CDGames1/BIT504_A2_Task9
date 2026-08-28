e5cb8b319e300e21f78beddce69a21ac9400b888
CDGames1 <63894306+CDGames1@users.noreply.github.com>
2026-08-28 16:00:10 +1200

    describe book service

--- src/Task9Demo.java
+++ src/Task9Demo.java
@@ -11,5 +11,6 @@
 public class Task9Demo {
     public static void main(String[] args) {
         System.out.println("BIT504 source control demonstration");
+        System.out.println("Books can be borrowed from the service desk");
     }
 }
