8f89d78c31490af0632fb92951ae9e4977bd0b50
Chiricahua Dilloway <63894306+CDGames1@users.noreply.github.com>
2026-08-28 22:11:49 +1200

    28 08 2026 added a function to display the opening hours

--- LibraryMessages.java
+++ LibraryMessages.java
@@ -5,9 +5,14 @@
 public class LibraryMessages {
     public static void showAll() {
         showWelcome();
+        showOpeningHours();
     }
 
     public static void showWelcome() {
         System.out.println("welcome to the local library");
+    }
+
+    public static void showOpeningHours() {
+        System.out.println("opening hours are 9 am to 5 pm");
     }
 }
