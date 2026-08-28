46a0fa3d7cf70340daaef34f0a6b617727b6d9af
Chiricahua Dilloway <63894306+CDGames1@users.noreply.github.com>
2026-08-28 22:11:39 +1200

    28 08 2026 added a function to display the welcome message

---
+++ LibraryMessages.java
@@ -0,0 +1,13 @@
+// name - Chiricahua Dilloway
+// Student ID -
+// Assessment 2 BIT504
+
+public class LibraryMessages {
+    public static void showAll() {
+        showWelcome();
+    }
+
+    public static void showWelcome() {
+        System.out.println("welcome to the local library");
+    }
+}
