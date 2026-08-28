19fad00476bb34d5f79ea9bc6bb6aa7e48d6f23d
Chiricahua Dilloway <63894306+CDGames1@users.noreply.github.com>
2026-08-28 22:11:27 +1200

    28 08 2026 created the java project and added the first display

---
+++ Task9Demo.java
@@ -0,0 +1,9 @@
+// name - Chiricahua Dilloway
+// Student ID -
+// Assessment 2 BIT504
+
+public class Task9Demo {
+    public static void main(String[] args) {
+        System.out.println("BIT504 task 9 source control");
+    }
+}
